bf58c5f4b485d3712bc581e3ee80da802f9eb2c0
Bhanu prakash reddy <141407774+Bhanu-0852@users.noreply.github.com>
2026-02-15 14:13:22 +0530

    Update index.js

--- src/components/TeamCard/index.js
+++ src/components/TeamCard/index.js
@@ -3,12 +3,12 @@ import './index.css'
 
 const TeamCard = props => {
   const {teamDetails} = props
-  const {id, name, teamImageUrl} = teamDetails
+  const {name, id, teamImageURL} = teamDetails
 
   return (
-    <li className="team-card">
-      <Link to={`/team-matches/${id}`} className="team-link">
-        <img src={teamImageUrl} alt={name} className="team-logo" />
+    <li className="team-item">
+      <Link to={`/team-matches/${id}`} className="link">
+        <img src={teamImageURL} alt={name} className="team-logo" />
         <p className="team-name">{name}</p>
       </Link>
     </li>
